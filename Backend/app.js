1eb92a7a90bb6386ab5cb0dff4ce5c89e697da12
Carlos Linares <Carlos Linares>
2016-01-18 16:39:33 -0600

    Se elimina caracter

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,6 @@
 // App.js
 // Conexion con la base de datos de mongo
 
-// 8 jitomates
-// brocoli
-// 
-
 var ws = require("ws").Server,
     wss = new ws({port:3000});
     
@@ -24,11 +20,12 @@ app.get("/", function(req, res){
 });
     
 var MongoClient = require("mongodb").MongoClient;
-
     
 MongoClient.connect('mongodb://localhost:27017/lakesh', function(err, db) { 
     if (err) { console.log("ERROR: " + err); }
     else {
         console.log("DONE: " + "conectado a Lakesh");
+        
+        
     }
 });
